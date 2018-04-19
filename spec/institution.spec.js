07f29e43a75f667a9facd96eb3c7b1bd78bce249
Elliott Grieco <elliott.grieco@gmail.com>
2018-04-19 13:09:30 -0400

    Refactors institution tests

--- spec/institution.spec.js
+++ spec/institution.spec.js
@@ -1,6 +1,6 @@
 const { LAMBDA, BASE_SEARCH_URL, INSTITUTIONS } = require("./helpers/constants");
 
-describe('persistent', () => {
+describe('basic persistent query', () => {
   describe("null query", () => {
     it('should redirect to NYU search', (done) => {
       return LAMBDA.event({
@@ -14,7 +14,21 @@ describe('persistent', () => {
     });
   });
 
-  describe('institution view', () => {
+  describe('institution view ONLY', () => {
+    it(`should account for mis-capitalization`, (done) => {
+      const institution = 'nYu';
+      return LAMBDA.event({
+        "queryStringParameters": {
+          institution
+        }
+      })
+      .expectResult(result => {
+        expect(result.statusCode).toEqual(302);
+        expect(result.headers.Location).toEqual(`${BASE_SEARCH_URL}&vid=${institution.toUpperCase()}`);
+      })
+      .verify(done);
+    });
+
     INSTITUTIONS.forEach((institution) => {
       it(`should redirect to ${institution} search`, (done) => {
         return LAMBDA.event({
