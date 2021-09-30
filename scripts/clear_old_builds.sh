bae7cd439a390c7451be06137730fe85372862ae
BAlter <barnaby.alter@nyu.edu>
2021-09-29 21:19:10 -0400

    Start prototyping a script to delete all but the latest deployments

---
+++ scripts/clear_old_builds.sh
@@ -0,0 +1,19 @@
+#!/bin/sh -e
+
+# : "${S3_BUCKET?Must specify S3_BUCKET}"
+S3_BUCKET="nyulibraries-dev-web-lambdas-code"
+LAMBDA_FN="on-campus-restricted-requests"
+
+aws s3 ls s3://${S3_BUCKET}/${LAMBDA_FN}/ --recursive | sort | tail -n 3 | awk '{print $4}' > exclude.txt
+
+EXCLUDE_ARGS=""
+EXCLUDE_FILES="$(cat exclude.txt)"
+
+for line in $EXCLUDE_FILES
+do
+    EXCLUDE_ARGS="$EXCLUDE_ARGS--exclude \"$(echo $line | awk -F'/' '{printf("%s/%s"), $2, $3}')\" "
+done 
+
+aws s3 rm s3://${S3_BUCKET}/${LAMBDA_FN}/ --recursive --dryrun ${EXCLUDE_ARGS}
+
+echo $EXCLUDE_ARGS
