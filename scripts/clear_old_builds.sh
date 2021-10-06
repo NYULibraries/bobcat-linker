18ec2bd484fa6e3c9c18ed1eef960818b6652e5e
BAlter <30699+barnabyalter@users.noreply.github.com>
2021-10-06 13:48:18 -0400

    Complete clear_old_builds script and add to circle pipeline

--- scripts/clear_old_builds.sh
+++ scripts/clear_old_builds.sh
@@ -1,8 +1,9 @@
-#!/bin/sh -e
+#!/bin/bash -e
 
-# : "${S3_BUCKET?Must specify S3_BUCKET}"
-S3_BUCKET="nyulibraries-dev-web-lambdas-code"
-LAMBDA_FN="on-campus-restricted-requests"
+: "${S3_BUCKET?Must specify S3_BUCKET}"
+: "${LAMBDA_FN?Must specify LAMBDA_FN}"
+# S3_BUCKET="nyulibraries-dev-web-lambdas-code"
+# LAMBDA_FN="on-campus-restricted-requests"
 
 aws s3 ls s3://${S3_BUCKET}/${LAMBDA_FN}/ --recursive | sort | tail -n 3 | awk '{print $4}' > exclude.txt
 
@@ -11,9 +12,8 @@ EXCLUDE_FILES="$(cat exclude.txt)"
 
 for line in $EXCLUDE_FILES
 do
-    EXCLUDE_ARGS="$EXCLUDE_ARGS--exclude \"$(echo $line | awk -F'/' '{printf("%s/%s"), $2, $3}')\" "
+    EXCLUDE_ARGS="$EXCLUDE_ARGS--exclude $(echo $line | awk -F'/' '{printf("%s/%s"), $2, $3}') "
 done 
 
-aws s3 rm s3://${S3_BUCKET}/${LAMBDA_FN}/ --recursive --dryrun ${EXCLUDE_ARGS}
 
-echo $EXCLUDE_ARGS
+aws s3 rm s3://${S3_BUCKET}/${LAMBDA_FN}/ --recursive --dryrun $EXCLUDE_ARGS
